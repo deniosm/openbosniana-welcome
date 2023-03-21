828a33edc0610b5ea9d8b549b88c5b8a48c4f325
denios <76059129+deniosm@users.noreply.github.com>
2023-03-21 02:40:44 +0100

    Update dconf , skel ,debian/* , grub ,udev-rules

--- debian/denios-core/usr/lib/denios-skel/etc/skel/Templates/prog/bash-sh.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-echo "Welcome to Denios Gnu/Linux"
--- debian/denios-core/usr/lib/denios-skel/etc/skel/Templates/prog/c++.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-
-using namespace std;
-
-main()
-{
-	cout << "Welcome to Denios Gnu/Linux" << endl;
-	return 0;
-}
